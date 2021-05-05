8eb33b7b3fb0659182b0a6b66e93f78545546b0c
mrjun27 <minh.hua.3720@gmail.com>
2021-05-05 11:44:29 +0700

    Add checkout

---
+++ src/app/_services/order.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Book } from '../models/book';
+import { ShoppingCartParam } from '../models/shoppingcartParam';
+import { User } from '../models/user';
+import { Cart } from '../models/cart';
+import { ShoppingCartUpdate } from '../models/shoppingCartUpdate';
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  cart: Cart[] = [];
+
+  baseUrl = environment.apiUrl;
+  constructor(private Http: HttpClient) { }
+  getCart(id: number) {
+    return this.Http.get<Cart>(this.baseUrl + "shoppingcart/" + id).pipe(
+      map(response => {
+        return response;
+      }))
+  }
+}
